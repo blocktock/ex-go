3d199721f3fa43064c7590707cf141f19d356cee
gaoyue <gaoyue@haochezhu.club>
2024-03-19 17:22:01 +0800

    up handle

--- internal/http/handle/user.handle.go
+++ internal/http/handle/user.handle.go
@@ -3,6 +3,7 @@ package handle
 import (
 	"ex-go/internal/domain/service"
 	"ex-go/internal/schema"
+	"github.com/blocktock/go-pkg/ginx"
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
 )
@@ -19,16 +20,16 @@ func (uc *UserHandle) CreateUser(c *gin.Context) {
 
 	// 解析 JSON 请求体
 	if err := c.ShouldBindJSON(&requestData); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		ginx.ResError(c, 400, err.Error())
 		return
 	}
 
 	// 调用服务层处理请求
 	err := uc.UserService.CreateUser(requestData)
 	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+		ginx.ResError(c, 500, err.Error())
 		return
 	}
 
-	c.JSON(200, gin.H{"message": "User created successfully"})
+	ginx.ResSuccess(c, "User created successfully")
 }
